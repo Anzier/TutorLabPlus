b9830d860ab92204551c207da9410972b08dc089
sjkelleyjr <sjkelleyjr@gmail.com>
2016-04-03 16:07:09 -0600

    changed colors, fixed button discrepencies, pulling from real database, changed authentication to last name rather than anumber

--- webApp/routes/index.js
+++ webApp/routes/index.js
@@ -44,179 +44,212 @@ router.post('/', function(req, res) {
 	}
 
 })
-router.get('/data/:id/:date', function(req,res){
-  //these queries need to be professor specific
-  var date = req.params.date
-  var querystring;
-  if(date.length > 2){
-      //query for +- 3 days from this date.
-      date = date.replace(/_/g, '/')
-      var upperDate = moment(date).format('MM/DD/YYYY');
-      upperDate = moment(upperDate).add(3,'day');
-      var lowerDate = moment(date).format('MM/DD/YYYY');
-      lowerDate = moment(lowerDate).subtract(3,'day');
-      upperDate = upperDate.format('MM/DD/');
-      lowerDate = lowerDate.format('MM/DD/');
-      //remove preceding 0's to format similar to SQL timeIn string
-      if(upperDate.charAt(0) === '0'){
-        upperDate = upperDate.substr(1);
-      }
-      if(lowerDate.charAt(0) === '0'){
-        lowerDate = lowerDate.substr(1);
-      }
-      querystring = 'select * from studentSessions where timeIn between \''+lowerDate+'%\' and \''+upperDate+'%\''
-    }else{
-      //query for month data
-      date++;
-      querystring = 'select * from studentSessions where timeIn like \''+date+'/%\'';
-  }
-  connection.query(querystring,function(err,data){
-      //here we need to build the JSON object based on the queries to make it look like fakeData
-      if(err) throw err;
-      console.log(data)
-  })
+//for individual queries
+// router.get('/data/:id/:date', function(req,res){
+//   //these queries need to be professor specific
+//   var date = req.params.date
+//   var querystring;
+//   if(date.length > 2){
+//       //query for +- 3 days from this date.
+//       date = date.replace(/_/g, '/')
+//       var upperDate = moment(date).format('MM/DD/YYYY');
+//       upperDate = moment(upperDate).add(3,'day');
+//       var lowerDate = moment(date).format('MM/DD/YYYY');
+//       lowerDate = moment(lowerDate).subtract(3,'day');
+//       upperDate = upperDate.format('MM/DD/');
+//       lowerDate = lowerDate.format('MM/DD/');
+//       //remove preceding 0's to format similar to SQL timeIn string
+//       if(upperDate.charAt(0) === '0'){
+//         upperDate = upperDate.substr(1);
+//       }
+//       if(lowerDate.charAt(0) === '0'){
+//         lowerDate = lowerDate.substr(1);
+//       }
+//       querystring = 'select * from studentSessions where timeIn between \''+lowerDate+'%\' and \''+upperDate+'%\''
+//     }else{
+//       //query for month data
+//       date++;
+//       querystring = 'select * from studentSessions where timeIn like \''+date+'/%\'';
+//   }
+//   connection.query(querystring,function(err,data){
+//       //here we need to build the JSON object based on the queries to make it look like fakeData
+//       if(err) throw err;
+//       console.log(data)
+//   })
+//   // CSP headers
+//   res.set("Access-Control-Allow-Origin", "*");
+//   res.set("Access-Control-Allow-Headers", "X-Requested-With");
+//   //here i'll create a json object with dates and a count and serve it up
+//   //NOTE: these NEED to be sorted on date before sending.
+//   var fakeData = {
+//     "Watson":
+//     [
+//       {
+//         "date":"20160104",
+//         "count":1
+//       },
+//       {
+//         "date":"20160105",
+//         "count":100
+//       },
+//       {
+//         "date":"20160301",
+//         "count":10
+//       },
+//       {
+//         "date":"20160302",
+//         "count":20
+//       },
+//       {
+//         "date":"20160303",
+//         "count":13
+//       },
+//       {
+//         "date":"20160304",
+//         "count":7
+//       },
+
+//       {
+//         "date":"20160305",
+//         "count":50
+//       },
+//       {
+//         "date":"20160306",
+//         "count":10
+//       },
+//       {
+//         "date":"20160313",
+//         "count":10
+//       },
+//       {
+//         "date":"20160314",
+//         "count":12
+//       },
+//       {
+//         "date":"20160315",
+//         "count":2
+//       },
+//       {
+//         "date":"20160322",
+//         "count":2
+//       },
+//       {
+//         "date":"20160323",
+//         "count":15
+//       },
+//     ]
+//   }
+//   res.send(fakeData);
+// })
+
+
+router.get('/data/:lname', function(req,res){
   // CSP headers
   res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Headers", "X-Requested-With");
-  //here i'll create a json object with dates and a count and serve it up
-  //NOTE: these NEED to be sorted on date before sending.
-  var fakeData = {
-    "Watson":
-    [
-      {
-        "date":"20160104",
-        "count":1
-      },
-      {
-        "date":"20160105",
-        "count":100
-      },
-      {
-        "date":"20160301",
-        "count":10
-      },
-      {
-        "date":"20160302",
-        "count":20
-      },
-      {
-        "date":"20160303",
-        "count":13
-      },
-      {
-        "date":"20160304",
-        "count":7
-      },
-
-      {
-        "date":"20160305",
-        "count":50
-      },
-      {
-        "date":"20160306",
-        "count":10
-      },
-      {
-        "date":"20160313",
-        "count":10
-      },
-      {
-        "date":"20160314",
-        "count":12
-      },
-      {
-        "date":"20160315",
-        "count":2
-      },
-      {
-        "date":"20160322",
-        "count":2
-      },
-      {
-        "date":"20160323",
-        "count":15
-      },
-    ]
-  }
-  res.send(fakeData);
-})
-
-
-router.get('/data/:id', function(req,res){
-  console.log(req.params)
+  var dates = [];
+  //console.log(req.params)
+  var querystring;
+  querystring = 'select * from studentSessions where teacher like \'%'+req.params.lname+'\'';
+  //console.log(querystring)
+  connection.query(querystring,function(err,data){
+      //here we need to build the JSON object based on the queries to make it look like fakeData
+      if(err) throw err;
+      //console.log(data)
+      //iterate the objects, if there is a date and its not in the map of dates, push it to the map of dates,
+      //if it is, increment its count
+      //console.log(data)
+      //sadly this code is going to need to go in the frontend
+      // console.log('SENDING');
+      // console.log(dates)
+      res.send(data);
+  })  
   //here query for all data associated with the professor
   //turn it into a JSON object like 'fakeData'
-	// CSP headers
-  res.set("Access-Control-Allow-Origin", "*");
-  res.set("Access-Control-Allow-Headers", "X-Requested-With");
   //here i'll create a json object with dates and a count and serve it up
   //NOTE: these NEED to be sorted on date before sending.
-  var fakeData = {
-  	"Watson":
-  	[
-   		{
-  			"date":"20160104",
-  			"count":1
-  		},
-   		{
-  			"date":"20160105",
-  			"count":100
-  		},
-  		{
-  			"date":"20160301",
-  			"count":10
-  		},
-  		{
-  			"date":"20160302",
-  			"count":20
-  		},
-  		{
-  			"date":"20160303",
-  			"count":13
-  		},
-  		{
-  			"date":"20160304",
-  			"count":7
-  		},
+  // var fakeData = {
+  // 	"Watson":
+  // 	[
+  //  		{
+  // 			"date":"20160104",
+  // 			"count":1
+  // 		},
+  //  		{
+  // 			"date":"20160105",
+  // 			"count":100
+  // 		},
+  // 		{
+  // 			"date":"20160301",
+  // 			"count":10
+  // 		},
+  // 		{
+  // 			"date":"20160302",
+  // 			"count":20
+  // 		},
+  // 		{
+  // 			"date":"20160303",
+  // 			"count":13
+  // 		},
+  // 		{
+  // 			"date":"20160304",
+  // 			"count":7
+  // 		},
 
-    	{
-  			"date":"20160305",
-  			"count":50
-  		},
-     	{
-  			"date":"20160306",
-  			"count":10
-  		},
-      {
-        "date":"20160313",
-        "count":10
-      },
-      {
-        "date":"20160314",
-        "count":12
-      },
-      {
-        "date":"20160315",
-        "count":2
-      },
-      {
-        "date":"20160322",
-        "count":2
-      },
-      {
-        "date":"20160323",
-        "count":15
-      },
-  	]
-  }
-	res.send(fakeData);
+  //   	{
+  // 			"date":"20160305",
+  // 			"count":50
+  // 		},
+  //    	{
+  // 			"date":"20160306",
+  // 			"count":10
+  // 		},
+  //     {
+  //       "date":"20160313",
+  //       "count":10
+  //     },
+  //     {
+  //       "date":"20160314",
+  //       "count":12
+  //     },
+  //     {
+  //       "date":"20160315",
+  //       "count":2
+  //     },
+  //     {
+  //       "date":"20160322",
+  //       "count":2
+  //     },
+  //     {
+  //       "date":"20160323",
+  //       "count":15
+  //     },
+  // 	]
+  // }
+	// res.send(fakeData);
 })
 
 router.get('/:lname', function(req,res){
-  //lets render something else here, get a fresh start to the page design
-	res.render('datapage',{
-		lname: JSON.stringify(req.params.lname)
-	});
+  var querystring;
+  querystring = 'select * from studentSessions where teacher like \'%'+req.params.lname+'\'';
+  //console.log(querystring)
+  connection.query(querystring,function(err,data){
+      //here we need to build the JSON object based on the queries to make it look like fakeData
+      if(err) throw err;
+      //console.log(data)
+      //iterate the objects, if there is a date and its not in the map of dates, push it to the map of dates,
+      //if it is, increment its count
+      //console.log(data)
+      //sadly this code is going to need to go in the frontend
+      // console.log('SENDING');
+      // console.log(dates)
+      // res.send(data);
+      res.render('datapage',{
+        lname: JSON.stringify(req.params.lname),
+        data:JSON.stringify(data)
+      });
+  }) 
+
 })
 
 module.exports = router
